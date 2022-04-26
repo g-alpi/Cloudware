420a670a0ee063b45c2fe2554a718ad035f7d3f9
Guillem Albo Pintor <albopintor@gmail.com>
2022-04-26 17:06:03 +0200

    BBDD creation

--- cloudwareApp/admin.py
+++ cloudwareApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Directory, File, ShareFile, ShareDirectory
 # Register your models here.
+
+class ShareFileAdmin (admin.ModelAdmin):
+    list_display = ('file_id', 'user_id')
+
+class ShareDirectoryAdmin (admin.ModelAdmin):
+    list_display = ('directoy_id', 'user_id')
+
+admin.site.register(Directory)
+admin.site.register(File)
+admin.site.register(ShareFile, ShareFileAdmin)
+admin.site.register(ShareDirectory, ShareDirectoryAdmin)
+
