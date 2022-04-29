a7ba55149982a11beadd85ad702f1d191234fbce
Carlos <carvagia92@gmail.com>
2022-04-29 19:33:33 +0200

    added base header and footer

--- cloudwareApp/urls.py
+++ cloudwareApp/urls.py
@@ -4,11 +4,13 @@ from .views import *
 app_name = 'cloud'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', landing_page, name='landing_page'),
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
     path('uploaded_files/<int:fileId>', downloadFile, name='downloadFile'),
     path('upload/', upload, name='upload'),
     path('upload-file/', save_file, name='upload-file'),
-    path('share-file/<int:fileId>', shareFile, name="shareFile")
+    path('share-file/<int:fileId>', shareFile, name="shareFile"),
 ]
 
 handler404 = "cloudwareApp.views.page_not_found"
