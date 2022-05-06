4ad4916568274bf31959903f419418b3ad2ee4fc
Carlos <carvagia92@gmail.com>
2022-05-06 16:47:13 +0200

    template done

--- cloudwareApp/static/cloudwareApp/profile.js
+++ cloudwareApp/static/cloudwareApp/profile.js
@@ -1,4 +1,25 @@
 $(".bg-semi-transparent-black").on("click", function(){
-    $(this).toggle();
-    $(".modify-form").toggle();
+    $(this).hide();
+    $(".modify-form").hide();
+});
+$("#edit-username").on("click", function(){
+    $("#modal-transparent-background").show();
+    $("#modify-username").show();
+});
+$("#edit-email").on("click", function(){
+    $("#modal-transparent-background").show();
+    $("#modify-email").show();
+});
+$("#edit-password").on("click", function(){
+    $("#modal-transparent-background").show();
+    $("#modify-password").show();
+});
+$("#delete-account").on("click", function(){
+    $("#modal-transparent-background").show();
+    $("#delete-account-modal").show();
+});
+$("dismiss-delete-account").on("click", function(event){
+    event.preventDefault();
+    $("#modal-transparent-background").hide();
+    $(".modify-form").hide();
 });
